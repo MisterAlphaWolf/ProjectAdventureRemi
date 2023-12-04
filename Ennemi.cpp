2b11d55e9a5dfce49f79451b681008efdaf6a9c3
Haroxgaming <mathis.denamur.pro@gmail.com>
2023-12-04 15:30:11 +0100

    Ennemi.h/.cpp setup (Random hp, xp, level, damage, defense)

--- Ennemi.cpp
+++ Ennemi.cpp
@@ -1,3 +1,59 @@
 #include "Ennemi.h"
 
-      
+
+int Ennemi::getHealth()
+{
+    return hp;
+}
+
+void Ennemi::setHealth(int h)
+{
+    std::random_device h;
+    std::uniform_int_distribution<int> dist(1, 20);
+
+}
+
+int Ennemi::getDamage()
+{
+    return damage;
+}
+
+void Ennemi::setDamage(int d)
+{
+    std::random_device d;
+    std::uniform_int_distribution<int> dist(1, 10);
+}
+
+int Ennemi::getArmor()
+{
+    return ca;
+}
+
+void Ennemi::setArmor(int a)
+{
+    std::random_device a;
+    std::uniform_int_distribution<int> dist(1, 10);
+}
+
+int Ennemi::getXPGive()
+{
+    return XPGive;
+}
+
+void Ennemi::setXPGive(int xp)
+{
+    std::random_device xp;
+    std::uniform_int_distribution<int> dist(1, 15);
+}
+
+int Ennemi::getZombieLevel()
+{
+    return 0;
+}
+
+void Ennemi::setZombieLevel(int l)
+{
+    std::random_device l;
+    std::uniform_int_distribution<int> dist(1, 5);
+}
+
--- Ennemi.h
+++ Ennemi.h
@@ -9,6 +9,36 @@ using namespace std;
 class Ennemi : public Personnage
 {
 
+private:
+
+	//Attribute (variables)
+	int hp;
+	int damage;
+	int ca;
+	int XPGive;
+	int ZombieLevel;
+
+public:
+
+	//Constructeur
+
+
+
+	//Getter Setter
+
+	int getHealth();
+	void setHealth(int h);
+
+	int getDamage();
+	void setDamage(int d);
+
+	int getArmor();
+	void setArmor(int a);
+
+	int getXPGive();
+	void setXPGive(int xp);
+
+	int getZombieLevel();
+	void setZombieLevel(int l);
 
 };
-
