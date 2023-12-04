3f5aaa643baed7c028f56365a1489ce809311a30
Sorakaii <60962735+Sorakaii@users.noreply.github.com>
2023-12-04 15:20:56 +0100

    add creating window method

--- Player.h
+++ Player.h
@@ -1,13 +1,31 @@
 #pragma once
 #include <iostream>
 #include <cstdlib>
+#include <Windows.h>
 #include <time.h>
+#include <vector>
 #include "Personnage.h"
+#include "Item.h"
 
-using namespace std;
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+//using namespace std;
 
-class Player
+class Player : public Personnage
 {
+	std::vector<Item> Inventory;
 
+	public :
+
+		Player(); 
+		Player(const Player&) = delete;
+		Player& operator =(const Player&) = delete;
+		~Player();
+
+
+		HINSTANCE m_hInstance;
+		HWND m_hWnd;
+
+		//Actions
+		bool OpenInventory();
 };
 
